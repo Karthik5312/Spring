adea960ca40d123bc466ae9c67a2cb7eaa58bee9
Karthik5312 <karthikprasad2k20@gmail.com>
2025-01-24 22:35:00 +0530

    added examples

---
+++ Spring Boot/example/src/main/java/com/karthik/example/ExampleApplication.java
@@ -0,0 +1,28 @@
+package com.karthik.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ExampleApplication {
+
+	public static void main(String[] args) {
+
+		var ctx = SpringApplication.run(ExampleApplication.class, args);
+
+		//MyFirstClass myFirstClass = new MyFirstClass();
+		//System.out.println(myFirstClass.sayHello());
+
+		MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class);
+		System.out.println(myFirstClass.sayHello());
+
+
+	}
+
+	@Bean
+	public MyFirstClass myFirstClass() {
+		return new MyFirstClass();
+	}
+
+}
---
+++ Spring Boot/example/src/main/java/com/karthik/example/MyFirstClass.java
@@ -0,0 +1,8 @@
+package com.karthik.example;
+
+public class MyFirstClass {
+
+    public String sayHello() {
+        return "This is from MyFirstClass";
+    }
+}
